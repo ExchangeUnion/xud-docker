6722841618bb521ef0ec0945dc3a5d95820a84fd
kilrau <kilian.rausch@gmail.com>
2019-05-09 16:07:03 +0200

    chore: moving folders

--- xud-simnet-fast/compress.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Compress btcd data"
-tar -c -f btcd/data.tar.gz -z -v -C ../xud-simnet/data/btcd/data/simnet/blocks_ffldb .
-
-echo "Compress ltcd data"
-tar -c -f ltcd/data.tar.gz -z -v -C ../xud-simnet/data/ltcd/data/simnet/blocks_ffldb .
