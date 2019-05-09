d5db8ddd2ff890297adba7643efa1226de6c6fe5
Yang Yang <reliveyy@gmail.com>
2019-05-09 22:07:06 +0800

    Update dist file

--- xud-simnet-fast/push.sh
+++ xud-simnet-fast/push.sh
@@ -4,8 +4,10 @@ docker tag xud-simnet-fast_btcd exchangeunion/btcd-simnet
 docker tag xud-simnet-fast_ltcd exchangeunion/ltcd-simnet
 docker tag xud-simnet-fast_lndbtc exchangeunion/lnd
 docker tag xud-simnet-fast_xud exchangeunion/xud:test
+docker tag xud-simnet-fast_ctl exchangeunion/ctl
 
 docker push exchangeunion/btcd-simnet
 docker push exchangeunion/ltcd-simnet
 docker push exchangeunion/lnd
 docker push exchangeunion/xud:test
+docker push exchangeunion/ctl
