73b2f2e3905f4ed4ddd5e29424939653af13c7d0
Yang Yang <reliveyy@gmail.com>
2019-06-10 09:29:07 +0800

    Add vagrant tests

---
+++ tests/init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Fail-fast
+set -e
+
+# Install docker-ce on Ubuntu
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker vagrant
+
+docker info
+
+# Install docker-compose
+# https://docs.docker.com/compose/install/
+sudo curl -sL "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
+
