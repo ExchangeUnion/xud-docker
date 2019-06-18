11bd7bb800947640f8f61674dd9018cacbc47352
Yang Yang <reliveyy@gmail.com>
2019-06-19 02:31:20 +0800

    Fix litecoind with safe wrapper

---
+++ images/litecoind/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# A wrapper around /entrypoint.sh to trap the SIGINT signal (Ctrl+C) and forwards it to the mysql daemon
+# In other words : traps SIGINT and SIGTERM signals and forwards them to the child process as SIGTERM signals
+
+signalListener() {
+    "$@" &
+    pid="$!"
+    trap "echo 'Stopping PID $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
+
+    # A signal emitted while waiting will make the wait command return code > 128
+    # Let's wrap it in a loop that doesn't end before the process is indeed stopped
+    while kill -0 $pid > /dev/null 2>&1; do
+        wait
+    done
+}
+
+signalListener litecoind $@
