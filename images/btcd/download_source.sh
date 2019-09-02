fe094704003d5b5b39cee186032e64957c7a466f
Yang Yang <reliveyy@gmail.com>
2019-09-02 22:41:48 +0800

    chore: remove unused btcd image folder

--- images/btcd/download_source.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-if [[ $1 == 'latest' ]]; then
-    git clone -b master https://github.com/btcsuite/btcd $GOPATH/src/github.com/btcsuite/btcd
-else
-    git clone -b v$1 https://github.com/btcsuite/btcd $GOPATH/src/github.com/btcsuite/btcd
-fi
