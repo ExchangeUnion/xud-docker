21d8a5847d10444138aedc1cdac4f997375353fc
kilrau <kilian.rausch@gmail.com>
2019-09-06 12:19:03 +0200

    fix: init testnet aliases

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ case $XUD_NETWORK in
         alias lndltc-lncli="docker-compose exec lndltc lncli -n mainnet -c litecoin"
         alias geth="docker-compose exec geth geth"
         ;;
-    mainnet)
+    testnet)
         alias bitcoin-cli="docker-compose exec bitcoind bitcoin-cli -testnet -rpcuser=xu -rpcpassword=xu"
         alias litecoin-cli="docker-compose exec litecoind litecoin-cli -testnet -rpcuser=xu -rpcpassword=xu"
         alias lndbtc-lncli="docker-compose exec lndbtc lncli -n testnet -c bitcoin"
