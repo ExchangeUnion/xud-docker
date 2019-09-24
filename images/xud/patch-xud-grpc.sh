dcd16eada7317878ca25dffc6a3862672f992c77
Yang Yang <reliveyy@gmail.com>
2019-09-25 01:36:00 +0800

    tmp: move xud Dockerfile redundant patching logic into patch-xud-grpc.sh

--- images/xud/patch-xud-grpc.sh
+++ images/xud/patch-xud-grpc.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-cd ~/xud-simnet/xud
-
 sed -Ei 's|gpr_log|/*gpr_log|g' node_modules/grpc/deps/grpc/src/core/lib/gpr/env_linux.cc
 sed -Ei 's|used);|used);*/|g' node_modules/grpc/deps/grpc/src/core/lib/gpr/env_linux.cc
 sed -Ei 's/static_library/static_library --grpc_alpine=true/g' node_modules/grpc/package.json
