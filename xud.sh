666f4e1fb52b5ed7faf7900b3d223a7060750ceb
Yang Yang <reliveyy@gmail.com>
2019-10-11 03:18:29 +0800

    feat: customize bitcoind, litecoind and geth data directories
    This closes #202.
    
    This commit adds 3 new command line options `--bitcoind-dir`, `--litecoind-dir` and `--geth-dir` to customize corresponding services' data directories. The implementation uses environment variables before `docker-compose` command to inject variables into docker-compose file.

--- xud.sh
+++ xud.sh
@@ -2,12 +2,36 @@
 
 set -euo pipefail
 
-branch=master
+BRANCH=master
 
-while getopts b: opt 2>/dev/null; do
-    case "$opt" in
-        b) branch=$OPTARG;;
-    esac
-done
+function parse_arguments() {
+    local OPTION VALUE
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        "-b" | "--branch")
+            if [[ $1 =~ = ]]; then
+                VALUE=$(echo "$1" | cut -d'=' -f2)
+            else
+                OPTION=$1
+                shift
+                if [[ $# -eq 0 || $1 =~ ^- ]]; then
+                    echo >&2 "❌ Missing option value: $OPTION"
+                    exit 1
+                fi
+                if ! curl -sf -o /dev/null https://api.github.com/repos/ExchangeUnion/xud-docker/git/refs/heads/$1; then
+                    echo >&2 "❌ Branch \"$1\" does not exist"
+                    exit 1
+                fi
+                VALUE=$1
+            fi
+            BRANCH=$VALUE
+            ;;
+        *)
+            shift
+        esac
+    done
+}
 
-bash <(curl -sf https://raw.githubusercontent.com/ExchangeUnion/xud-docker/$branch/setup.sh) $@
+parse_arguments "$@"
+
+bash <(curl -sf "https://raw.githubusercontent.com/ExchangeUnion/xud-docker/$BRANCH/setup.sh") "$@"
