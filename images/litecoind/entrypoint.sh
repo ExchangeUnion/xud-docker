94f115ca35c220754e66f7e24d21b1db601161fa
Yang Yang <reliveyy@gmail.com>
2019-11-13 19:29:54 +0800

    fix: logfile not existed

--- images/litecoind/entrypoint.sh
+++ images/litecoind/entrypoint.sh
@@ -10,12 +10,14 @@ elif [[ $NETWORK == "mainnet" ]]; then
     LOGFILE=$LITECOIN_DIR/debug.log
 fi
 
-ERROR=$(tail -50 $LOGFILE | grep -A 2 ReadBlockFromDisk)
-if [[ -n $ERROR ]]; then
-    if [[ $(echo "$ERROR" | sed -n '2p') =~ "Failed to read block" && $(echo "$ERROR" | sed -n '3p') =~ "A fatal internal error occurred" ]]; then
-        # shellcheck disable=SC2068
-        litecoind -reindex-chainstate $@
-        exit $?
+if [[ -e $LOGFILE ]]; then
+    ERROR=$(tail -50 $LOGFILE | grep -A 2 ReadBlockFromDisk)
+    if [[ -n $ERROR ]]; then
+        if [[ $(echo "$ERROR" | sed -n '2p') =~ "Failed to read block" && $(echo "$ERROR" | sed -n '3p') =~ "A fatal internal error occurred" ]]; then
+            # shellcheck disable=SC2068
+            litecoind -reindex-chainstate $@
+            exit $?
+        fi
     fi
 fi
 
