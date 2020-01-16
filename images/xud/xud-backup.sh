c180e0111299940f8451ef20f0b7c007ebffb671
Yang Yang <reliveyy@gmail.com>
2020-01-16 23:05:53 +0800

    fix: create a db link until that db file shows up

--- images/xud/xud-backup.sh
+++ images/xud/xud-backup.sh
@@ -2,11 +2,6 @@
 
 RAIDEN_DB_PATH="/root/.raiden/.xud-backup-raiden-db"
 
-while [[ ! -e $RAIDEN_DB_PATH ]]; do
-  echo "[BACKUP] Waiting for raiden db file"
-  sleep 3
-done
-
 if [[ -d /root/.xud-backup ]]; then
     ./bin/xud-backup -b /root/.xud-backup --raiden.dbpath="$RAIDEN_DB_PATH"
 else
