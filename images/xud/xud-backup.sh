596485ec0e338ca162de004f554491a68388e4f8
Yang Yang <reliveyy@gmail.com>
2020-01-16 21:12:40 +0800

    fix: wait db file

--- images/xud/xud-backup.sh
+++ images/xud/xud-backup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+RAIDEN_DB_PATH="/root/.raiden/.xud-backup-raiden-db"
+
+while [[ ! -e $RAIDEN_DB_PATH ]]; do
+  echo "[BACKUP] Waiting for raiden db file"
+  sleep 3
+done
+
 if [[ -d /root/.xud-backup ]]; then
-    ./bin/xud-backup -b /root/.xud-backup --raiden.dbpath="/root/.raiden/.xud-backup-raiden-db"
+    ./bin/xud-backup -b /root/.xud-backup --raiden.dbpath="$RAIDEN_DB_PATH"
 else
     perl -MPOSIX -e '$0="xud-backup"; pause'
 fi
