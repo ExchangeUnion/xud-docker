d1a7ef6196b3d05baa140c2ff1108ace7ea16233
Yang Yang <reliveyy@gmail.com>
2020-02-16 00:22:06 +0800

    feat: enhance utils updating logic

--- setup.sh
+++ setup.sh
@@ -53,17 +53,115 @@ function choose_network() {
     shopt -u nocasematch
 }
 
-function ensure_utils_image() {
-    UTILS_IMAGE="exchangeunion/utils"
+function get_token() {
+    curl -sf "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$1:pull" | sed -E 's/^.*"token":"([^,]*)",.*$/\1/g'
+}
 
-    if [[ $BRANCH != "master" ]]; then
-        UTILS_IMAGE="exchangeunion/utils:latest__${BRANCH//\//-}"
+function get_image_metadata() {
+    local TOKEN
+    local NAME
+    local TAG
+    NAME=$(echo "$1" | cut -d':' -f1)
+    TAG=$(echo "$1" | cut -d':' -f2)
+    TOKEN=$(get_token "$NAME")
+    curl -sf -H "Authorization: Bearer $TOKEN" "https://registry-1.docker.io/v2/$NAME/manifests/$TAG"
+}
+
+function get_cloud_image() {
+    local RESP
+    RESP=$(get_image_metadata "$1")
+    if [[ -z $RESP ]]; then
+        return
+    fi
+    RESP=$(echo "$RESP" | grep v1Compatibility | head -1 | sed 's/^.*v1Compatibility":/printf/g' || echo "")
+    if [[ -z $RESP ]]; then
+        return
+    fi
+    RESP=$(eval "$RESP")
+    echo "$RESP" | sed -E 's/.*sha256:([a-z0-9]+).*/\1/g'
+    echo "$RESP" | sed -E 's/.*com.exchangeunion.image.created":"([^"]*)".*/\1/g'
+}
+
+function get_local_image() {
+    if ! docker image inspect -f '{{.Config.Image}}' "$1" 2>/dev/null | sed -E 's/sha256://g'; then
+        return
+    fi
+    docker image inspect -f '{{index .Config.Labels "com.exchangeunion.image.created"}}' "$1"
+}
+
+function get_image_status() {
+    # possible return values: up-to-date, outdated, newer, missing, local-only
+    local LOCAL CLOUD
+    local L_SHA256 C_SHA256
+    local L_CREATED C_CREATED
+    LOCAL=$(get_local_image "$1")
+    if [[ -z $LOCAL ]]; then
+        echo "missing"
+        return
     fi
 
-    if ! docker pull "$UTILS_IMAGE" >/dev/null 2>&1; then
-        echo >&2 "Failed to pull $UTILS_IMAGE"
+    CLOUD=$(get_cloud_image "$1")
+    if [[ -z $CLOUD ]]; then
+        echo "local-only"
+        return
+    fi
+
+    L_SHA256=$(echo "$LOCAL" | sed -n '1p')
+    C_SHA256=$(echo "$CLOUD" | sed -n '1p')
+
+    if [[ $L_SHA256 == "$C_SHA256" ]]; then
+        echo "up-to-date"
+        return
+    fi
+
+    L_CREATED=$(echo "$LOCAL" | sed -n '2p')
+    C_CREATED=$(echo "$CLOUD" | sed -n '2p')
+
+    if [[ $L_CREATED > $C_CREATED ]]; then
+        echo "newer"
+    else
+        echo "outdated"
+    fi
+}
+
+function pull_image() {
+    echo "Pulling image $1"
+    if ! docker pull "$1" >/dev/null 2>&1; then
+        echo >&2 "❌ Failed to pull image $1"
         exit 1
     fi
+}
+
+function retag_branch_image() {
+    local IMG
+    if [[ $1 =~ __ ]]; then
+        IMG=$(echo "$1" | sed -E 's/__.*//g')
+        if ! docker tag "$1" "$IMG" >/dev/null 2>&1; then
+            echo >&2 "❌ Failed to re-tag image from $1 to $IMG"
+            exit 1
+        fi
+    fi
+}
+
+function ensure_utils_image() {
+    local P_IMG # The image to pull
+    local STATUS
+
+    if [[ $BRANCH == "master" ]]; then
+        P_IMG="exchangeunion/utils:latest"
+    else
+        P_IMG="exchangeunion/utils:latest__${BRANCH//\//-}"
+    fi
+
+    STATUS=$(get_image_status "$P_IMG")
+
+    case $STATUS in
+        missing|outdated)
+            pull_image "$P_IMG"
+            ;;
+    esac
+
+    retag_branch_image "$P_IMG"
 }
 
 # shellcheck disable=SC2068
@@ -76,7 +174,7 @@ ensure_utils_image
 docker run --rm \
 -e PROG="$0" \
 --entrypoint args_parser \
-$UTILS_IMAGE \
+exchangeunion/utils \
 $@
 
 choose_network
@@ -99,7 +197,7 @@ VARS="$(docker run --rm \
 -e HOME_DIR="$HOME_DIR" \
 -e NETWORK="$NETWORK" \
 --entrypoint config_parser \
-$UTILS_IMAGE \
+exchangeunion/utils \
 $@)"
 
 eval "$VARS"
@@ -164,5 +262,5 @@ docker run --rm -it \
 -e BACKUP_DIR="$BACKUP_DIR" \
 -e RESTORE_DIR="$RESTORE_DIR" \
 --entrypoint python \
-$UTILS_IMAGE \
+exchangeunion/utils \
 -m launcher $@
