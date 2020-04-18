fd0d8b7566a331410c5f36f7cb64b64bff71d1b0
Yang Yang <reliveyy@gmail.com>
2020-04-18 13:58:30 +0800

    add listpeers check for lnd connect

--- images/lnd/0.9.2-beta-patched/entrypoint.sh
+++ images/lnd/0.9.2-beta-patched/entrypoint.sh
@@ -6,12 +6,13 @@ set -o pipefail
 set -o monitor # -m
 
 if [[ $CHAIN != "bitcoin" ]]; then
-    echo "[ERROR] Invalid chain: $CHAIN"
+    echo "[entrypoint] Invalid chain: $CHAIN"
     exit 1
 fi
 
 LND_DIR=$HOME/.lnd
 TOR_DIR=$LND_DIR/tor
+TOR_LOG=$LND_DIR/tor.log
 TOR_DATA_DIR=$LND_DIR/tor-data
 LND_HOSTNAME="$TOR_DIR/hostname"
 P2P_PORT=29375
@@ -24,22 +25,38 @@ HiddenServicePort $P2P_PORT 127.0.0.1:$P2P_PORT
 HiddenServiceVersion 3
 EOF
 
-tor -f /etc/tor/torrc &
+tor -f /etc/tor/torrc >"$TOR_LOG" 2>&1 &
 
 while [[ ! -e $LND_HOSTNAME ]]; do
-    echo "[DEBUG] Waiting for lndbtc onion address"
+    echo "[entrypoint] Waiting for lndbtc onion address"
     sleep 1
 done
 
 LND_ADDRESS=$(cat "$LND_HOSTNAME")
-echo "[DEBUG] Onion address for lndbtc is $LND_ADDRESS"
+echo "[entrypoint] Onion address for lndbtc is $LND_ADDRESS"
 
 function connect() {
-    lncli -n simnet -c bitcoin connect 02db09dd366d7ba6d061502b5b6db1bbb47c0daacd36fc399ab617fd6406cf822a@xud1.simnet.exchangeunion.com:10012
+    local key="02db09dd366d7ba6d061502b5b6db1bbb47c0daacd36fc399ab617fd6406cf822a"
+    local uri="$key@xud1.simnet.exchangeunion.com:10012"
+    while true; do
+        echo "[entrypoint] Connecting to $uri"
+        if lncli -n simnet -c bitcoin connect $uri >/dev/null 2>&1; then
+            if lncli -n simnet -c bitcoin listpeers | grep -q $key; then
+                echo "[entrypoint] Connected to $uri"
+                break
+            fi
+        fi
+        sleep 5
+    done
 }
 
-(sleep 30 && connect) &
+connect &
 
+
+while ! nc -z 127.0.0.1 9050; do
+    echo "[entrypoint] Waiting for Tor port 9050 to be open"
+    sleep 1
+done
 
 lnd --externalip=$LND_ADDRESS:$P2P_PORT \
 --listen=0.0.0.0:$P2P_PORT \
