aceb72b666e9862d02db578aaab4782225895f51
Yang Yang <reliveyy@users.noreply.github.com>
2020-04-20 20:52:33 +0800

    feat: use xud default ports like 8886, 18886, 28886 (#395)
    * use xud default ports like 8886, 18886, 28886
    
    * fix xucli create
    
    * remove --rpcport for xucli
    
    * use xud:latest (master) in testnet
    
    * Update images/xud/latest/xud-backup.sh
    
    Co-Authored-By: Kilian Rausch ⚡️ <kilian.rausch@gmail.com>
    
    Co-authored-by: Kilian Rausch ⚡️ <kilian.rausch@gmail.com>

---
+++ images/xud/latest/xud-backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+RAIDEN_DB_PATH="/root/.raiden/.xud-backup-raiden-db"
+BACKUP_DIR_VALUE_PATH="/root/.xud/.backup-dir-value"
+
+while [[ ! -e $BACKUP_DIR_VALUE_PATH ]]; do
+    echo "[xud-backup] Waiting for backup_dir value"
+    sleep 1
+done
+
+BACKUP_DIR=$(cat "$BACKUP_DIR_VALUE_PATH")
+
+function check_backup_dir() {
+    if [[ ! -e $1 ]]; then
+        echo "[xud-backup] $BACKUP_DIR does not exist"
+        return 1
+    fi
+
+    if [[ ! -d $1 ]]; then
+        echo "[xud-backup] $BACKUP_DIR is not a directory"
+        return 1
+    fi
+
+    if [[ ! -r $1 ]]; then
+        echo "[xud-backup] $BACKUP_DIR is not readable"
+        return 1
+    fi
+
+    if [[ ! -w $1 ]]; then
+        echo "[xud-backup] $BACKUP_DIR is not writable"
+        return 1
+    fi
+
+    return 0
+}
+
+while ! check_backup_dir "$BACKUP_DIR"; do
+    sleep 5
+done
+
+./bin/xud-backup -b "$BACKUP_DIR" --raiden.dbpath="$RAIDEN_DB_PATH"
