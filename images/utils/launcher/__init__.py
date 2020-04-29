227d8c5a829598da023688059633a0e3af3c1230
Yang Yang <reliveyy@gmail.com>
2020-04-29 15:40:10 +0800

    fix typo

--- images/utils/launcher/__init__.py
+++ images/utils/launcher/__init__.py
@@ -109,7 +109,7 @@ your issue.""")
 
     def pre_start(self):
         if self.config.network in ["simnet", "testnet", "mainnet"]:
-            time.sleep(5)  # cool down 5 second in case lnd unlock stuck
+            time.sleep(5)  # cool down 5 seconds in case lnd unlock stuck
             self.check_wallets()
         if self.config.network == "simnet":
             self.wait_for_channels()
