6583a478c9ccfe05b25f5b485fceda6760e6dbe0
Yang Yang <reliveyy@gmail.com>
2020-06-09 23:22:37 +0800

    add arby to testnet and mainnet too

--- images/utils/launcher/node/arby.py
+++ images/utils/launcher/node/arby.py
@@ -13,25 +13,22 @@ class Arby(Node):
     def __init__(self, name, ctx):
         super().__init__(name, ctx)
 
-        if self.network == "simnet":
-            api_key = self.node_config["binance-api-key"] \
-                if "binance-api-key" in self.node_config else ""
-            api_secret = self.node_config["binance-api-secret"] \
-                if "binance-api-secret" in self.node_config else ""
-            margin = self.node_config["margin"] \
-                if "margin" in self.node_config else ""
-            environment = [
-                "LOG_LEVEL=debug",
-                "DATA_DIR=/root/.arby",
-                "OPENDEX_CERT_PATH=/root/.xud/tls.cert",
-                "OPENDEX_RPC_HOST=xud",
-                "OPENDEX_RPC_PORT=28886",
-                f'BINANCE_API_SECRET={api_secret}',
-                f'BINANCE_API_KEY={api_key}',
-                f'MARGIN={margin}',
-            ]
-        else:
-            environment = []
+        api_key = self.node_config["binance-api-key"] \
+            if "binance-api-key" in self.node_config else ""
+        api_secret = self.node_config["binance-api-secret"] \
+            if "binance-api-secret" in self.node_config else ""
+        margin = self.node_config["margin"] \
+            if "margin" in self.node_config else ""
+        environment = [
+            "LOG_LEVEL=debug",
+            "DATA_DIR=/root/.arby",
+            "OPENDEX_CERT_PATH=/root/.xud/tls.cert",
+            "OPENDEX_RPC_HOST=xud",
+            "OPENDEX_RPC_PORT=28886",
+            f'BINANCE_API_SECRET={api_secret}',
+            f'BINANCE_API_KEY={api_key}',
+            f'MARGIN={margin}',
+        ]
 
         self.container_spec.environment.extend(environment)
 
