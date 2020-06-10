deda623ffed57614ee96183880403dd096edd942
Yang Yang <reliveyy@gmail.com>
2020-06-10 13:49:10 +0800

    chore(xud): upgrade to 1.0.0-beta.4

--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -343,7 +343,7 @@ nodes_config = {
         },
         "xud": {
             "name": "xud",
-            "image": "exchangeunion/xud:1.0.0-beta.3",
+            "image": "exchangeunion/xud:1.0.0-beta.4",
             "volumes": [
                 {
                     "host": "$data_dir/xud",
