24b726aba4d0b048ce517c316531bf964e312233
michael1011 <me@michael1011.at>
2020-06-12 10:11:45 +0200

    feat: add Boltz daemon

---
+++ images/boltz/latest/wrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CHAIN=$1
+
+case "$CHAIN" in
+    "btc")
+        PORT="9002"
+        ;;
+
+    "ltc")
+        PORT="9003"
+        ;;
+
+    # Print the help command
+    "help")
+        exec boltzcli help
+        exit 0
+        ;;
+
+    # Handle missing/wrong chains
+    "")
+        echo "Missing chain"
+        exit 1
+        ;;
+
+    *)
+        echo "Chain $CHAIN not supported"
+        exit 1
+        ;;
+esac
+
+exec boltzcli --port $PORT ${@:2}
