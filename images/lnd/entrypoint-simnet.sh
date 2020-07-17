8c34a80c62de1fbddb145243c9acf21f453d0fa9
Yang Yang <reliveyy@users.noreply.github.com>
2020-07-17 22:13:16 +0800

    fix: simnet lndbtc (#606)

--- images/lnd/entrypoint-simnet.sh
+++ images/lnd/entrypoint-simnet.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
-set -o errexit # -e
-set -o nounset # -u
-set -o pipefail
-
-LND_DIR=$HOME/.lnd
-P2P_PORT=9735
-
-
-TOR_DIR=$LND_DIR/tor
-TOR_LOG=$LND_DIR/tor.log
-TOR_DATA_DIR=$LND_DIR/tor-data
-LND_HOSTNAME="$TOR_DIR/hostname"
-
-
-[[ -e /etc/tor/torrc ]] || cat <<EOF >/etc/tor/torrc
-DataDirectory $TOR_DATA_DIR#!/bin/bash
-
 set -o errexit # -e
 set -o nounset # -u
 set -o pipefail
@@ -87,39 +68,6 @@ done
 # use exec to properly respond to SIGINT
 exec lnd --externalip=$LND_ADDRESS:$P2P_PORT \
 --listen=0.0.0.0:$P2P_PORT \
---rpclisten=0.0.0.0:10009 \
---restlisten=0.0.0.0:8080 \
---trickledelay=1 \
---tor.active \
---tor.socks=9050 \
---tor.streamisolation \
-$@
-
-ExitPolicy reject *:* # no exits allowed
-HiddenServiceDir $TOR_DIR
-HiddenServicePort $P2P_PORT 127.0.0.1:9735
-HiddenServiceVersion 3
-EOF
-
-tor -f /etc/tor/torrc >"$TOR_LOG" 2>&1 &
-
-while [[ ! -e $LND_HOSTNAME ]]; do
-    echo "[entrypoint] Waiting for lndbtc onion address"
-    sleep 1
-done
-
-LND_ADDRESS=$(cat "$LND_HOSTNAME")
-echo "[entrypoint] Onion address for lndbtc is $LND_ADDRESS"
-
-
-while ! nc -z 127.0.0.1 9050; do
-    echo "[entrypoint] Waiting for Tor port 9050 to be open"
-    sleep 1
-done
-
-# use exec to properly respond to SIGINT
-exec lnd --externalip=$LND_ADDRESS:$P2P_PORT \
---listen=0.0.0.0:9735 \
 --rpclisten=0.0.0.0:10009 \
 --restlisten=0.0.0.0:8080 \
 --trickledelay=1 \
