acb09bad0eeb1df67c2cefc0f140e51e23d1c35b
Karl Ranna <karl@karlranna.com>
2020-07-20 19:51:58 +0300

    upgrade simnet connext (#610)

--- images/connext/src.py
+++ images/connext/src.py
@@ -8,6 +8,6 @@ class SourceManager(src.SourceManager):
     def get_ref(self, version):
         if version == "latest":
             # connext commit lock
-            return "fe1876a0411e0b9928825ae51104e16a9fd7b787"
+            return "d3649f6085e41693c52eb49a8a178401211d5126"
         elif version == "7.0.0-alpha.14":
             return "fe1876a0411e0b9928825ae51104e16a9fd7b787"
