78a2a5139aa7a37335ef9b496b1cf99611e33f17
kilrau <19181985+kilrau@users.noreply.github.com>
2020-07-23 19:06:04 +0200

    chore: updgrade connext 7.0.0-alpha.14 -> 7.0.0

--- images/connext/src.py
+++ images/connext/src.py
@@ -9,5 +9,5 @@ class SourceManager(src.SourceManager):
         if version == "latest":
             # connext commit lock
             return "08a453eac82b4c01ff9e85a7780b2ab89c7e9081"
-        elif version == "7.0.0-alpha.14":
-            return "fe1876a0411e0b9928825ae51104e16a9fd7b787"
+        elif version == "7.0.0":
+            return "08a453eac82b4c01ff9e85a7780b2ab89c7e9081"
