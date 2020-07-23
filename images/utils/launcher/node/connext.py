df2823faf14adc482ea71435ac75c38432ce3015
kilrau <19181985+kilrau@users.noreply.github.com>
2020-07-23 19:04:09 +0200

    feat: new connext URLs

--- images/utils/launcher/node/connext.py
+++ images/utils/launcher/node/connext.py
@@ -30,11 +30,11 @@ class Connext(Node):
             ]
         elif self.network == "testnet":
             environment = [
-                "CONNEXT_NODE_URL=https://connext.testnet.odex.dev",
+                "CONNEXT_NODE_URL=https://connext.testnet.boltz.exchange",
             ]
         elif self.network == "mainnet":
             environment = [
-                "CONNEXT_NODE_URL=https://connext.odex.dev/api",
+                "CONNEXT_NODE_URL=https://connext.boltz.exchange",
             ]
 
         if self.network in ["testnet", "mainnet"]:
