fd9d38c5ee7e009166d75215ee4ccbcd5ee3cbc3
Kilian Rausch ⚡️ <19181985+kilrau@users.noreply.github.com>
2020-07-30 11:41:24 +0200

    chore: upgrade connext client (#631)

--- images/connext/src.py
+++ images/connext/src.py
@@ -8,6 +8,6 @@ class SourceManager(src.SourceManager):
     def get_ref(self, version):
         if version == "latest":
             # connext commit lock
-            return "08a453eac82b4c01ff9e85a7780b2ab89c7e9081"
+            return "0ae160790faa6b5bc6b33114d726e1a3eb2fcfa3"
         elif version == "7.0.0":
             return "08a453eac82b4c01ff9e85a7780b2ab89c7e9081"
