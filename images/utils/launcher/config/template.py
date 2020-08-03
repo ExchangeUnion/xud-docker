1fca599bacc2be97e4c2670b38a54341a73b7149
Kilian Rausch ⚡️ <19181985+kilrau@users.noreply.github.com>
2020-08-03 18:19:23 +0200

    chore: upgrade arby 1.1.0 -> 1.1.1 (#641)

--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -451,7 +451,7 @@ nodes_config = {
         },
         "arby": {
             "name": "arby",
-            "image": "exchangeunion/arby:1.1.0",
+            "image": "exchangeunion/arby:1.1.1",
             "volumes": [
                 {
                     "host": "$data_dir/arby",
