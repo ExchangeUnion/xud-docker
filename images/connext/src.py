139ec927f4f1d49ce62acfd9b590fd9cc67fe034
raladev <aladev.r@gmail.com>
2020-08-05 14:17:54 +0300

    Update connect commit hash for latest version

--- images/connext/src.py
+++ images/connext/src.py
@@ -8,6 +8,6 @@ class SourceManager(src.SourceManager):
     def get_ref(self, version):
         if version == "latest":
             # connext commit lock
-            return "0ae160790faa6b5bc6b33114d726e1a3eb2fcfa3"
+            return "5127ad848c3f598a9f23c51dc6d491753740832d"
         elif version == "7.0.0":
             return "08a453eac82b4c01ff9e85a7780b2ab89c7e9081"
