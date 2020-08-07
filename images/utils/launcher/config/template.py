d17583c412211c664fdb387203e40131a90308f7
Yang Yang <reliveyy@users.noreply.github.com>
2020-08-07 19:30:08 +0800

    chore: release version 20.08.07 (#652)

--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -437,7 +437,7 @@ nodes_config = {
         },
         "connext": {
             "name": "connext",
-            "image": "exchangeunion/connext:7.0.0",
+            "image": "exchangeunion/connext:7.1.0",
             "volumes": [
                 {
                     "host": "$data_dir/connext",
