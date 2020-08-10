f6534dea53015a4037f63c309dd13ff31193b724
Karl Ranna <karl@karlranna.com>
2020-08-10 09:30:03 +0000

    feat: upgrade connext to 7.1.1

--- images/connext/src.py
+++ images/connext/src.py
@@ -7,8 +7,10 @@ class SourceManager(src.SourceManager):
 
     def get_ref(self, version):
         if version == "latest":
-            return "26c521ac62181c1d1945a3e252351e24beee8096"
+            return "16fa7aecc19f44dab12e14b9264df63fccb25585"
         elif version == "7.0.0":
             return "5127ad848c3f598a9f23c51dc6d491753740832d"
         elif version == "7.1.0":
             return "26c521ac62181c1d1945a3e252351e24beee8096"
+        elif version == "7.1.1":
+            return "16fa7aecc19f44dab12e14b9264df63fccb25585"
--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -437,7 +437,7 @@ nodes_config = {
         },
         "connext": {
             "name": "connext",
-            "image": "exchangeunion/connext:7.1.0",
+            "image": "exchangeunion/connext:7.1.1",
             "volumes": [
                 {
                     "host": "$data_dir/connext",
