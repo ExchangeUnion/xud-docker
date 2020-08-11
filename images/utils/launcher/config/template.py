42e65e4fca779b45977125d23eb1f05dcb0dfffa
Karl Ranna <karl@karlranna.com>
2020-08-11 13:59:47 +0300

    feat: upgrade connext to 7.1.2 (#654)
    * feat: upgrade connext to 7.1.1
    
    * chore: bump connext client to 7.1.2
    
    Co-authored-by: kilrau <19181985+kilrau@users.noreply.github.com>

--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -437,7 +437,7 @@ nodes_config = {
         },
         "connext": {
             "name": "connext",
-            "image": "exchangeunion/connext:7.1.0",
+            "image": "exchangeunion/connext:7.1.2",
             "volumes": [
                 {
                     "host": "$data_dir/connext",
