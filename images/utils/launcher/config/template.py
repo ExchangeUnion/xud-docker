248634fdce57e0a8b25a0cfa5a3a4d59ab1f2c51
Karl Ranna <karl@karlranna.com>
2020-08-17 10:52:25 +0000

    debug: use connext:latest for mainnet

--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -437,7 +437,7 @@ nodes_config = {
         },
         "connext": {
             "name": "connext",
-            "image": "exchangeunion/connext:1.3.0",
+            "image": "exchangeunion/connext:latest",
             "volumes": [
                 {
                     "host": "$data_dir/connext",
