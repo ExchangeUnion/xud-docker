aaf87d787f53a3cde1f4a2b6aa797e50a8949cf3
Yang Yang <reliveyy@gmail.com>
2020-09-03 22:26:21 +0800

    add container_spec test

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,5 +1,16 @@
+import pytest
 from subprocess import check_output
 import json
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def build_utils():
+    os.system("tools/build utils")
+    wd = os.getcwd()
+    os.chdir("tests")
+    yield
+    os.chdir(wd)
 
 
 def test_xud_options():
@@ -7,3 +18,4 @@ def test_xud_options():
     output = check_output(cmd, shell=True)
     j = json.loads(output)
     print(j)
+    assert "XUD_OPTS=--foo bar" in j["environment"]
--- tests/utils_test/dump_node_container_spec.py
+++ tests/utils_test/dump_node_container_spec.py
@@ -1,8 +1,40 @@
-from unittest.mock import patch
+from typing import Any
+from unittest.mock import patch, MagicMock
+import docker.errors
+import json
+from launcher.node.base import ContainerSpec
+from launcher.node.image import Image
+from launcher.node.xud import Xud
+from launcher.config import Config
 
 
-def fake_from_env():
-    print("fake")
+def get_image(name):
+    raise docker.errors.ImageNotFound(name)
+
+
+def fake_from_env(timeout=None):
+    mock = MagicMock()
+    mock.images.get = get_image
+    return mock
+
+
+class MyEncoder(json.JSONEncoder):
+
+    def default(self, o: Any) -> Any:
+        if isinstance(o, ContainerSpec):
+            d = vars(o)
+            return d
+        elif isinstance(o, Image):
+            d = vars(o)
+            d.pop("logger")
+            return d
+        elif isinstance(o, Xud):
+            return "xud"
+        elif isinstance(o, MagicMock):
+            return "mock"
+        elif isinstance(o, Config):
+            return "config"
+        return super().default(o)
 
 
 with patch("docker.from_env", fake_from_env):
@@ -13,5 +45,5 @@ with patch("docker.from_env", fake_from_env):
     env = XudEnv(config, None)
     xud = env.node_manager.get_node("xud")
     spec = xud.container_spec
-    s = json.dumps(spec)
+    s = json.dumps(spec, cls=MyEncoder)
     print(s)
