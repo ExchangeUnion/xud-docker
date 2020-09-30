7b6d0e1e14e5999720f9f34b665ec04bd27a3894
Karl Ranna <karl@karlranna.com>
2020-09-30 09:24:36 +0000

    use master branch of arby

--- images/arby/src.py
+++ images/arby/src.py
@@ -13,7 +13,6 @@ class SourceManager(src.SourceManager):
 
     def get_ref(self, version):
         if version == "latest":
-            # change "master" to a another xud branch for testing
-            return "feat/config-defaults"
+            return "master"
         else:
             return "v" + version
