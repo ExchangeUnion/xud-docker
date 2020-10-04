eab2eed002d26fed6b9c7fd393ebb25783e514ca
Kilian Rausch | ⚡ OpenDEX ⚡ <19181985+kilrau@users.noreply.github.com>
2020-10-04 14:57:18 +0200

    chore: upgrade lndbtc 0.11.0 -> 0.11.1, upgrade geth 1.9.20 -> 1.9.22 (#744)

--- images/lndbtc-simnet/src.py
+++ images/lndbtc-simnet/src.py
@@ -25,6 +25,6 @@ class SourceManager(src.SourceManager):
 
     def get_ref(self, version):
         if version == "latest":
-            return "v0.11.0-beta"
+            return "v0.11.1-beta"
         else:
             return super().get_ref(version)
