1a147a6309ba2a049d9543d4bc0d0e5cd81ed727
Yang Yang <reliveyy@gmail.com>
2020-10-06 15:43:27 +0800

    forward xud.sh params to setup.sh

--- xud.sh
+++ xud.sh
@@ -32,12 +32,17 @@ parse_arguments "$@"
 
 URL="https://raw.githubusercontent.com/ExchangeUnion/xud-docker/$BRANCH/setup.sh"
 
+if ! command -v curl >/dev/null; then
+    echo >&2 "Command \"curl\" not found"
+    exit 1
+fi
+
 if SCRIPT=$(curl -s "$URL"); then
     if [[ "$SCRIPT" == "404: Not Found" ]]; then
         echo >&2 "Xud-docker branch \"$BRANCH\" does not exist"
         exit 1
     fi
-    bash -c "$SCRIPT"
+    bash -c "$SCRIPT" -- "$@"
 else
     EXIT_CODE="$?"
     case "$EXIT_CODE" in
