f6629eb0daafcbbd1491fc5fd5e6d439df4b52bf
Yang Yang <reliveyy@gmail.com>
2020-10-08 17:27:00 +0800

    change proxy repo url

--- images/proxy/src.py
+++ images/proxy/src.py
@@ -3,7 +3,7 @@ from tools.core import src
 
 class SourceManager(src.SourceManager):
     def __init__(self):
-        super().__init__("https://github.com/ExchangeUnion/xud-docker-api-poc")
+        super().__init__("https://github.com/ExchangeUnion/xud-docker-api")
 
     def get_ref(self, version):
         if version == "latest":
