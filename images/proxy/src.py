fda4b1d7f82d2ecbc663144bbe4f5270b458d69d
krr prr <24500376+krrprr@users.noreply.github.com>
2020-11-06 21:11:17 +0200

    feat(proxy): add ui dashboard app to proxy container

--- images/proxy/src.py
+++ images/proxy/src.py
@@ -1,13 +1,26 @@
 from tools.core import src
+import os
 
 
 class SourceManager(src.SourceManager):
     def __init__(self):
-        super().__init__("https://github.com/ExchangeUnion/xud-docker-api")
+        super().__init__(None)
+        self.frontend_dir = os.path.join(self.src_dir, "frontend")
+        self.backend_dir = os.path.join(self.src_dir, "backend")
 
-    def get_ref(self, version):
+    def ensure(self, version):
+        self.ensure_repo("https://github.com/ExchangeUnion/xud-ui-dashboard", self.frontend_dir)
+        self.ensure_repo("https://github.com/ExchangeUnion/xud-docker-api", self.backend_dir)
         if version == "latest":
-            # change "master" to a another xud branch for testing
-            return "master"
+            # change "master" or "main" to a another xud branch for testing
+            self.checkout_repo(self.frontend_dir, "main")
+            self.checkout_repo(self.backend_dir, "master")
         else:
-            return "v" + version
+            self.checkout_repo(self.frontend_dir, "v" + version)
+            self.checkout_repo(self.backend_dir, "v" + version)
+
+    def get_application_revision(self, version):
+        r1 = self.get_revision(self.frontend_dir)
+        r2 = self.get_revision(self.backend_dir)
+        return f"frontend:{r1},backend:{r2}"
+
