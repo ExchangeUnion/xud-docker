479562cef100073294749e35daf18d49e3552330
Yang Yang <reliveyy@gmail.com>
2020-11-10 19:19:07 +0800

    use proxy:latest on mainnet for testing

--- images/utils/launcher/config/template.py
+++ images/utils/launcher/config/template.py
@@ -595,7 +595,7 @@ nodes_config = {
         },
         "proxy": {
             "name": "proxy",
-            "image": "exchangeunion/proxy:1.0.0",
+            "image": "exchangeunion/proxy:latest",
             "volumes": [
                 {
                     "host": "/var/run/docker.sock",
