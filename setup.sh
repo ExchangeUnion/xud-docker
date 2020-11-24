7f6f5b7809b391220c450ddc29632097833ad7e9
Yang Yang <reliveyy@users.noreply.github.com>
2020-11-24 20:44:26 +0800

    chore: release version 20.11.24 (#798)

--- setup.sh
+++ setup.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 BRANCH=master
 DEV=false
 DOCKER_REGISTRY="https://registry-1.docker.io"
-UTILS_TAG="20.11.19"
+UTILS_TAG="20.11.24"
 
 
 function print_help() {
     cat <<EOF
-xud.sh 20.11.19
+xud.sh 20.11.24
 The launcher script for Exchange Union environment
 
 USAGE:
