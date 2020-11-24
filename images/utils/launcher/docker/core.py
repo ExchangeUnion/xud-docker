9b3174d0fb616c27675fdd80928c255fe0decb4b
Yang Yang <reliveyy@gmail.com>
2020-11-24 23:44:22 +0800

    refine the wall of conditional statements
    This commit refines the wall of conditional statements in `pull_image` function by adding some explanatory comments.

--- images/utils/launcher/docker/core.py
+++ images/utils/launcher/docker/core.py
@@ -1,12 +1,12 @@
 import json
+import logging
 import platform
 import re
 import urllib.error
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from http import HTTPStatus
-from typing import Optional, Any, Dict, List
-import logging
+from typing import Optional, Any, Dict, List, Callable
 
 import docker
 import docker.errors
@@ -18,6 +18,8 @@ from .registry import DockerRegistryClient
 __all__ = (
     "DockerUtility"
 )
+
+UpdateFunc = Optional[Callable[[str, Optional[str], Optional[float]], str]]
 
 
 @dataclass
@@ -180,39 +182,62 @@ class DockerUtility:
             raise UnsupportedArchitecture(m)
         return target
 
-    def pull_image(self, name: str, update=None):
+    def pull_image(self, name: str, update: UpdateFunc = None) -> str:
+        """Pull image with in-time updates
+
+        :param name: the pulling image name
+        :param update: the update callback function
+        :return: pulled image digest
+        """
         canonical_name = self.parse_image_name(name)
         assert canonical_name.tag
 
         digest = None
 
-        for payload in self.docker_client.api.pull(canonical_name.repo, tag=canonical_name.tag, stream=True, decode=True):
-            self._logger.debug("Pulling %s: %s", name, payload)
-            if update:
-                status = payload["status"]
+        for payload in self.docker_client.api.pull(
+                canonical_name.repo,
+                tag=canonical_name.tag, stream=True, decode=True):
 
-                if "id" in payload:
-                    id = payload["id"]
-                    if status == "Downloading":
-                        detail = payload["progressDetail"]
-                        if update:
-                            update("downloading", id, detail["current"] / detail["total"])
-                    elif status == "Extracting":
-                        detail = payload["progressDetail"]
-                        if update:
-                            update("downloading", id, 1)
-                            update("extracting", id, detail["current"] / detail["total"])
-                    elif status == "Pull complete":
-                        if update:
-                            update("extracting", id, 1)
-                else:
-                    if status.startswith("Digest: "):
-                        digest = status.replace("Digest: ", "")
-                    elif status.startswith("Status: Downloaded newer image for"):
-                        if update:
-                            update("done", None, None)
-                    elif status.startswith("Status: Image is up to date for"):
-                        if update:
-                            update("up-to-date", None, None)
+            self._logger.debug("Pulling %s: %s", name, payload)
+
+            status = payload["status"]
+
+            if "id" in payload:
+                # handle layer events
+
+                id = payload["id"]
+                if status == "Downloading":
+                    # {'status': 'Downloading', 'progressDetail': {'current': 79985079, 'total': 82104778},
+                    #   'progress': '[================================================>  ]  79.99MB/82.1MB',
+                    #   'id': '9f8de6cacc4d'}
+                    detail = payload["progressDetail"]
+                    if callable(update):
+                        update("downloading", id, detail["current"] / detail["total"])
+                elif status == "Extracting":
+                    # {'status': 'Extracting', 'progressDetail': {'current': 63504384, 'total': 75341762},
+                    #   'progress': '[==========================================>        ]   63.5MB/75.34MB',
+                    #   'id': 'c843ec64a111'}
+                    detail = payload["progressDetail"]
+                    if callable(update):
+                        update("downloading", id, 1.0)
+                        update("extracting", id, detail["current"] / detail["total"])
+                elif status == "Pull complete":
+                    # {'status': 'Pull complete', 'progressDetail': {}, 'id': 'c843ec64a111'}
+                    if callable(update):
+                        update("extracting", id, 1.0)
+            else:
+                # handle general events
+
+                if status.startswith("Digest: "):
+                    # {'status': 'Digest: sha256:33e4fa7ea4f719b069be3603d09c49010863a9f7d739a7a5f1bf161c73262f51'}
+                    digest = status.replace("Digest: ", "")
+                elif status.startswith("Status: Downloaded newer image for"):
+                    # {'status': 'Status: Downloaded newer image for connextproject/vector_node:837bafa1'}
+                    if callable(update):
+                        update("done", None, None)
+                elif status.startswith("Status: Image is up to date for"):
+                    # {'status': 'Status: Image is up to date for connextproject/vector_node:837bafa1'}
+                    if callable(update):
+                        update("up-to-date", None, None)
 
         return digest
