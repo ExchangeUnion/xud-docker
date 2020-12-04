41be16226afa55bd72abd9a09629b2c2e5028ac4
michael1011 <me@michael1011.at>
2020-12-04 12:26:08 +0100

    fix: correct commit version of Boltz binaries (#809)

--- images/boltz/src.py
+++ images/boltz/src.py
@@ -1,1 +1,19 @@
-REPO_URL = "https://github.com/BoltzExchange/boltz-lnd"
+from tools.core import src
+
+
+class SourceManager(src.SourceManager):
+    def __init__(self):
+        super().__init__("https://github.com/BoltzExchange/boltz-lnd")
+
+    def get_build_args(self, version):
+        revision = self.get_revision(self.src_dir)
+        print(revision[:7])
+        return {
+            "GIT_REVISION": revision[:7]
+        }
+
+    def get_ref(self, version):
+        if version == "latest":
+            return "master"
+        else:
+            return "v" + version
